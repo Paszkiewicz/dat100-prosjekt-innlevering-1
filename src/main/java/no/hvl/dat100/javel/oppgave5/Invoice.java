6740bf38a86bf1f89cd6d8eca9abe8a9da14307d
Markus Hovring <hovringmarkus@gmail.com>
2025-10-21 21:39:56 +0200

    Update Invoice.java

--- src/main/java/no/hvl/dat100/javel/oppgave5/Invoice.java
+++ src/main/java/no/hvl/dat100/javel/oppgave5/Invoice.java
@@ -11,25 +11,46 @@ public class Invoice {
     private String month; // month that the invoice covers
     private double[][] usage; // power usage this month (per day and per hour)
     private double[][] prices; // power prices for this month
-
     private double amount; // power price for this month
 
 
     public Invoice(Customer c, String month, double[][] usage, double[][] power_prices) {
-
-        // TODO - konstruktør
-
+        this.c = c;
+        this.month = month;
+        this.usage = usage;
+        this.prices = power_prices;
+        this.amount = 0.0;
     }
 
     public void computeAmount() {
-
-        // TODO
-
+        double total = 0.0;
+        for (int i = 0; i < usage.length; i++) {
+            for (int j = 0; j < usage[i].length; j++) {
+                total += usage[i][j] * prices[i][j];
+            }
+        }
+        this.amount = total;
     }
 
-    public void printInvoice() {
 
-        // TODO
+    public void printInvoice() {
+        double totalusage = 0.0;
+        for (int i = 0; i < usage.length; i++) {
+            for (int j = 0; j < usage[i].length; j++) {
+                totalusage += usage[i][j];
+            }
+        }
+
+        System.out.println("========================");
+        System.out.println("Customer number " + c.getNumber());
+        System.out.println("Name  " + c.getName());
+        System.out.println("Email " + c.getEmail());
+        System.out.println("Agreement " + c.getAgreement());
+        System.out.println();
+        System.out.println("Month: " + month);
+        System.out.printf("Usage:      %.2f kWh%n", totalUsage);
+        System.out.printf("Amount:    %.2f NOK%n", amount);
+        System.out.println("========================");
 
     }
 }
