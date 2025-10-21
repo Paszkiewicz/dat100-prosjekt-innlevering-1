9cd3eba07d68deed7808dffd1476a59ff9e6399d
Bartosz Paszkiewicz <bartosz@paszkiewicz.no>
2025-10-21 23:43:27 +0200

    Start på Oppgave 4

--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -13,6 +13,11 @@ public class CustomerMain {
 
         Customer customer = new Customer("Potato Hansen", "potato@hansen.no", 1001, PowerAgreementType.SPOTPRICE);
 
-        System.out.print(customer);
+        System.out.println(customer);
+        System.out.println();
+        System.out.println(customer.getCustomer_id());
+        System.out.println(customer.getName());
+        System.out.println(customer.getEmail());
+        System.out.println(customer.getAgreement());
     }
 }
--- src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
+++ src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
@@ -8,30 +8,33 @@ public class Customers {
 
     // a) Complete constructor
     public Customers(int size) {
-
-        // TODO
-
+        this.customers = new Customer[size];
     }
 
     // b) count number of non-null references
     public int countNonNull() {
 
-
         int count = 0;
 
-        // TODO
-
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i] != null) {
+                count++;
+            }
+        }
         return count;
     }
 
     // c) return reference to customer with given id (if exists)
     public Customer getCustomer(int customer_id) {
 
-        boolean funnet = false;
+       // boolean funnet = false;
         Customer c = null;
 
-        // TODO
-
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i].getCustomer_id() == customer_id){
+                c = customers[i];
+            }
+        }
         return c;
     }
 
